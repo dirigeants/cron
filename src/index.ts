f5cfaec50af8a54ebb7ea913069a43eb0358dd28
bdistin <bdistin@gmail.com>
2019-10-01 14:10:47 -0500

    Rewrite Cron to TS with tests

---
+++ src/index.ts
@@ -0,0 +1,113 @@
+import { partRegex, wildcardRegex, allowedNum, predefined, tokens, tokensRegex } from './constants';
+
+/**
+ * Handles Cron strings and generates dates based on the cron string provided.
+ * @see https://en.wikipedia.org/wiki/Cron
+ */
+export class Cron {
+
+	public cron: string;
+	public normalized: string;
+	public minutes: number[];
+	public hours: number[];
+	public days: number[];
+	public months: number[];
+	public dows: number[];
+
+	/**
+	 * @param cron The cron pattern to use
+	 */
+	public constructor(cron: string) {
+		this.cron = cron.toLowerCase();
+		this.normalized = this.constructor._normalize(this.cron);
+		[this.minutes, this.hours, this.days, this.months, this.dows] = this.constructor._parseString(this.normalized);
+	}
+
+	/**
+	 * Get the next date that matches with the current pattern
+	 * @param outset The Date instance to compare with
+	 * @param origin Whether this next call is origin
+	 */
+	public next(outset: Date = new Date(), origin: boolean = true): Date {
+		if (!this.days.includes(outset.getUTCDate()) || !this.months.includes(outset.getUTCMonth() + 1) || !this.dows.includes(outset.getUTCDay())) {
+			return this.next(new Date(outset.getTime() + DAY), false);
+		}
+		if (!origin) return new Date(Date.UTC(outset.getUTCFullYear(), outset.getUTCMonth(), outset.getUTCDate(), this.hours[0], this.minutes[0]));
+
+		const now = new Date(outset.getTime() + 60000);
+
+		for (const hour of this.hours) {
+			if (hour < now.getUTCHours()) continue;
+			for (const minute of this.minutes) {
+				if (hour === now.getUTCHours() && minute < now.getUTCMinutes()) continue;
+				return new Date(Date.UTC(outset.getUTCFullYear(), outset.getUTCMonth(), outset.getUTCDate(), hour, minute));
+			}
+		}
+
+		return this.next(new Date(outset.getTime() + DAY), false);
+	}
+
+	/**
+	 * Normalize the pattern
+	 * @param cron The pattern to normalize
+	 */
+	private static _normalize(cron: string): string {
+		if (cron in predefined) return predefined[cron];
+		const now = new Date();
+		cron = cron.split(' ').map((val, i) => val.replace(wildcardRegex, match => {
+			if (match === 'h') return Math.floor(Math.random() * (allowedNum[i][1] + 1));
+			if (match === '?') {
+				switch (i) {
+					case 0: return now.getUTCMinutes();
+					case 1: return now.getUTCHours();
+					case 2: return now.getUTCDate();
+					case 3: return now.getUTCMonth();
+					case 4: return now.getUTCDay();
+				}
+			}
+			return match;
+		})).join(' ');
+		return cron.replace(tokensRegex, match => tokens[match]);
+	}
+
+	/**
+	 * Parse the pattern
+	 * @param cron The pattern to parse
+	 */
+	private static _parseString(cron: string): Array<number[]> {
+		const parts = cron.split(' ');
+		if (parts.length !== 5) throw new Error('Invalid Cron Provided');
+		return parts.map(Cron._parsePart);
+	}
+
+	/**
+	 * Parse the current part
+	 * @param cronPart The part of the pattern to parse
+	 * @param id The id that identifies the current part
+	 */
+	private static _parsePart(cronPart: string, id: number): number[] {
+		if (cronPart.includes(',')) {
+			const res = [];
+			for (const part of cronPart.split(',')) res.push(...Cron._parsePart(part, id));
+			return [...new Set(res)].sort((a, b) => a - b);
+		}
+
+		// eslint-disable-next-line prefer-const
+		let [, wild, min, max, step] = partRegex.exec(cronPart);
+
+		if (wild) [min, max] = allowedNum[id];
+		else if (!max && !step) return [parseInt(min)];
+		return Cron._range(...[parseInt(min), parseInt(max) || allowedNum[id][1]].sort((a, b) => a - b), parseInt(step) || 1);
+	}
+
+	/**
+	 * Get an array of numbers with the selected range
+	 * @param min The minimum value
+	 * @param max The maximum value
+	 * @param step The step value
+	 */
+	private static _range(min: number, max: number, step: number): number[] {
+		return new Array(Math.floor((max - min) / step) + 1).fill(0).map((val, i) => min + (i * step));
+	}
+
+}
--- test/Util.ts
+++ test/Util.ts
@@ -1,4 +1,199 @@
 import ava from 'ava';
-import Util from '../dist';
+import { Cron } from '../dist';
 
-//... others
+ava('pre-defined @hourly', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('@hourly');
+
+    test.is(specimine.normalized, '0 * * * *');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('pre-defined @daily', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('@daily');
+
+    test.is(specimine.normalized, '0 0 * * *');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('pre-defined @weekly', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('@weekly');
+
+    test.is(specimine.normalized, '0 0 * * 0');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0]);
+});
+
+ava('pre-defined @monthly', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('@monthly');
+
+    test.is(specimine.normalized, '0 0 1 * *');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('pre-defined @yearly', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('@yearly');
+
+    test.is(specimine.normalized, '0 0 1 1 *');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1]);
+    test.deepEqual(specimine.months, [1]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('pre-defined @annually', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('@annually');
+
+    test.is(specimine.normalized, '0 0 1 1 *');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1]);
+    test.deepEqual(specimine.months, [1]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('every minute', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('* * * * *');
+
+    test.is(specimine.normalized, '* * * * *');
+    test.deepEqual(specimine.minutes, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]);
+    test.deepEqual(specimine.hours, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('every other minute', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('*/2 * * * *');
+
+    test.is(specimine.normalized, '*/2 * * * *');
+    test.deepEqual(specimine.minutes, [0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58]);
+    test.deepEqual(specimine.hours, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('every other hour', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('* */2 * * *');
+
+    test.is(specimine.normalized, '* */2 * * *');
+    test.deepEqual(specimine.minutes, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]);
+    test.deepEqual(specimine.hours, [0,2,4,6,8,10,12,14,16,18,20,22]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('every other day', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('* * */2 * *');
+
+    test.is(specimine.normalized, '* * */2 * *');
+    test.deepEqual(specimine.minutes, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]);
+    test.deepEqual(specimine.hours, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+    test.deepEqual(specimine.days, [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('every other month', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('* * * */2 *');
+
+    test.is(specimine.normalized, '* * * */2 *');
+    test.deepEqual(specimine.minutes, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]);
+    test.deepEqual(specimine.hours, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,3,5,7,9,11]);
+    test.deepEqual(specimine.dows, [0,1,2,3,4,5,6]);
+});
+
+ava('every other day of week', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('* * * * */2');
+
+    test.is(specimine.normalized, '* * * * */2');
+    test.deepEqual(specimine.minutes, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]);
+    test.deepEqual(specimine.hours, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [0,2,4,6]);
+});
+
+ava('every monday through friday token at midnight', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('0 0 * * mon-fri');
+
+    test.is(specimine.normalized, '0 0 * * 1-5');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [1,2,3,4,5,6,7,8,9,10,11,12]);
+    test.deepEqual(specimine.dows, [1,2,3,4,5]);
+});
+
+ava('every friday in july at midnight', (test): void => {
+    test.plan(6);
+
+    const specimine = new Cron('0 0 * jul fri');
+
+    test.is(specimine.normalized, '0 0 * 7 5');
+    test.deepEqual(specimine.minutes, [0]);
+    test.deepEqual(specimine.hours, [0]);
+    test.deepEqual(specimine.days, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]);
+    test.deepEqual(specimine.months, [7]);
+    test.deepEqual(specimine.dows, [5]);
+});
+
+ava('this instant', (test): void => {
+    test.plan(6);
+
+    const now = new Date();
+    const [min, hour, day, month, dow] = [now.getUTCMinutes(), now.getUTCHours(), now.getUTCDate(), now.getUTCMonth(), now.getUTCDay()];
+    const specimine = new Cron('? ? ? ? ?');
+
+    test.is(specimine.normalized, `${min} ${hour} ${day} ${month} ${dow}`);
+    test.deepEqual(specimine.minutes, [min]);
+    test.deepEqual(specimine.hours, [hour]);
+    test.deepEqual(specimine.days, [day]);
+    test.deepEqual(specimine.months, [month]);
+    test.deepEqual(specimine.dows, [dow]);
+});
