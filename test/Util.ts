5edf5629cbd071a66bec415dd2c1fafebd7dbda5
Antonio Román <kyradiscord@gmail.com>
2020-05-13 16:37:31 +0200

    chore(): updated `.github/` and `.npmignore` (#146)

--- test/Util.ts
+++ test/Util.ts
@@ -107,6 +107,19 @@ ava('every other minute', (test): void => {
 	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
 });
 
+ava('every seventh minute starting at the second', (test): void => {
+	test.plan(6);
+
+	const specimine = new Cron('1/7 * * * *');
+
+	test.is(specimine.normalized, '1/7 * * * *');
+	test.deepEqual(specimine.minutes, [1, 8, 15, 22, 29, 36, 43, 50, 57]);
+	test.deepEqual(specimine.hours, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
+	test.deepEqual(specimine.months, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
+});
+
 ava('every other hour', (test): void => {
 	test.plan(6);
 
@@ -115,6 +128,19 @@ ava('every other hour', (test): void => {
 	test.is(specimine.normalized, '* */2 * * *');
 	test.deepEqual(specimine.minutes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]);
 	test.deepEqual(specimine.hours, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]);
+	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
+	test.deepEqual(specimine.months, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
+});
+
+ava('every seventh hour starting at the second', (test): void => {
+	test.plan(6);
+
+	const specimine = new Cron('* 1/7 * * *');
+
+	test.is(specimine.normalized, '* 1/7 * * *');
+	test.deepEqual(specimine.minutes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]);
+	test.deepEqual(specimine.hours, [1, 8, 15, 22]);
 	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
 	test.deepEqual(specimine.months, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
@@ -133,6 +159,19 @@ ava('every other day', (test): void => {
 	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
 });
 
+ava('every seventh day starting at the second', (test): void => {
+	test.plan(6);
+
+	const specimine = new Cron('* * 1/7 * *');
+
+	test.is(specimine.normalized, '* * 1/7 * *');
+	test.deepEqual(specimine.minutes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]);
+	test.deepEqual(specimine.hours, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+	test.deepEqual(specimine.days, [1, 8, 15, 22, 29]);
+	test.deepEqual(specimine.months, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
+});
+
 ava('every other month', (test): void => {
 	test.plan(6);
 
@@ -143,6 +182,19 @@ ava('every other month', (test): void => {
 	test.deepEqual(specimine.hours, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
 	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
 	test.deepEqual(specimine.months, [1, 3, 5, 7, 9, 11]);
+	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
+});
+
+ava('every seventh month starting at the second', (test): void => {
+	test.plan(6);
+
+	const specimine = new Cron('* * * 1/7 *');
+
+	test.is(specimine.normalized, '* * * 1/7 *');
+	test.deepEqual(specimine.minutes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]);
+	test.deepEqual(specimine.hours, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
+	test.deepEqual(specimine.months, [1, 8]);
 	test.deepEqual(specimine.dows, [0, 1, 2, 3, 4, 5, 6]);
 });
 
@@ -157,6 +209,19 @@ ava('every other day of week', (test): void => {
 	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
 	test.deepEqual(specimine.months, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 	test.deepEqual(specimine.dows, [0, 2, 4, 6]);
+});
+
+ava('every other day of week starting at the second', (test): void => {
+	test.plan(6);
+
+	const specimine = new Cron('* * * * 1/2');
+
+	test.is(specimine.normalized, '* * * * 1/2');
+	test.deepEqual(specimine.minutes, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]);
+	test.deepEqual(specimine.hours, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]);
+	test.deepEqual(specimine.days, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]);
+	test.deepEqual(specimine.months, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+	test.deepEqual(specimine.dows, [1, 3, 5]);
 });
 
 ava('every monday through friday token at midnight', (test): void => {
@@ -213,6 +278,28 @@ ava('this instant', (test): void => {
 	test.deepEqual(specimine.dows, [dow]);
 });
 
+ava('any instant', (test): void => {
+	test.plan(5);
+
+	const specimine = new Cron('h h h h h');
+	const [minutes, hours, date, month, day] = specimine.normalized.split(' ').map(Number);
+
+	test.true(minutes >= 0 && minutes <= 59);
+	test.true(hours >= 0 && hours <= 23);
+	test.true(date >= 1 && date <= 31);
+	test.true(month >= 1 && month <= 12);
+	test.true(day >= 0 && day <= 6);
+});
+
+// Saturday 9th March 2019, at 16:20:35:500
+const testDate = new Date(2019, 2, 9, 16, 20, 35, 500);
+
+ava('next minute', (test): void => {
+	const specimine = new Cron('* * * * *');
+	const next = specimine.next(testDate);
+
+	test.is(next.getTime(), new Date(2019, 2, 9, 16, 21, 0, 0).getTime());
+});
 
 ava('bad cron', (test): void => {
 	test.throws(() => new Cron('? ?'), { message: 'Invalid Cron Provided' });
